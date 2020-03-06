1407657bd317772417f3945f3c21546f3927d7f1
Alexander Popov <alex.popov.tech@gmail.com>
2020-03-06 23:09:47 +0200

    fix windows geckodriver unzip, refactoring
    Signed-off-by: Alexander Popov <alex.popov.tech@gmail.com>

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-const { DOWNLOAD_OUTPUT_PATH, SELENOID_PATH, SELENOID_CONFIG_PATH, CHROMEDRIVER_PATH, GECKODRIVER_PATH, OPERADRIVER_PATH } = require('../constants');
-const { start } = require('../start');
-const { update } = require('../update');
+const { DOWNLOAD_OUTPUT_PATH, SELENOID_PATH, SELENOID_CONFIG_PATH, CHROMEDRIVER_PATH, GECKODRIVER_PATH, OPERADRIVER_PATH } = require('../lib/constants');
+const { start } = require('../lib/start');
+const { update } = require('../lib/update');
 
 require('yargs')
   .command({
@@ -20,9 +20,16 @@ require('yargs')
   .command({
     command: 'update',
     desc: 'Update selenoid and drivers',
-    handler: () => {
+    handler: async () => {
       try {
-        update({ downloadOutputPath: DOWNLOAD_OUTPUT_PATH, selenoidPath: SELENOID_PATH, selenoidConfigPath: SELENOID_CONFIG_PATH, chromedriverPath: CHROMEDRIVER_PATH, geckodriverPath: GECKODRIVER_PATH, operadriverPath: OPERADRIVER_PATH })
+        await update({
+          downloadOutputPath: DOWNLOAD_OUTPUT_PATH,
+          selenoidPath: SELENOID_PATH,
+          selenoidConfigPath: SELENOID_CONFIG_PATH,
+          chromedriverPath: CHROMEDRIVER_PATH,
+          geckodriverPath: GECKODRIVER_PATH,
+          operadriverPath: OPERADRIVER_PATH
+        })
       } catch (err) {
         console.error(err.message);
         process.exit(1);
